f39edcfdc446593d47b92257ae82057f06f51af0
thedaruma <rgiorda1@gmail.com>
2023-09-04 15:54:38 -0400

    create heirarchy of planets and moons

--- Assets/Scripts/Revolve.cs
+++ Assets/Scripts/Revolve.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Revolve : MonoBehaviour
 {
     public Vector3 orbitAxis = new Vector3(0, 1, 0.5f);
+    public float orbitSpeed = 36f;
     public Transform parent;
     public Transform child;
     // Start is called before the first frame update
@@ -17,9 +18,7 @@ public class Revolve : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float speed = 36f;
-        float angle = speed * Time.deltaTime;
-        Debug.Log(angle);
+        float angle = orbitSpeed * Time.deltaTime;
         child.RotateAround(parent.position, parent.forward, angle);
     }
 }

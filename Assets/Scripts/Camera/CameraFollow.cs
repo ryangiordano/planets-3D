bbc6834386a881500ace3f7eb2a270aad03d653e
thedaruma <rgiorda1@gmail.com>
2023-09-04 19:56:03 -0400

    add camera follow logic

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothTime = 0.3f;
+
+    public float distance = 10;
+
+    private Vector3 velocity;
+
+    void LateUpdate()
+    {
+
+        // Get target's current position
+        Vector3 targetPos = target.position;
+        // Offset to maintain distance
+        targetPos += -transform.forward * distance;
+        // Smoothly interpolate camera position
+        transform.position = Vector3.SmoothDamp(
+          transform.position,
+          targetPos,
+          ref velocity,
+          smoothTime
+        );
+
+    }
+
+}

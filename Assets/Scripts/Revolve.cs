09f1f6f7ee92c71f8861c6e7e2b948b37ba111ee
thedaruma <rgiorda1@gmail.com>
2023-09-04 15:35:53 -0400

    add planetary rotation

--- Assets/Scripts/Revolve.cs
+++ Assets/Scripts/Revolve.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Revolve : MonoBehaviour
 {
+    public Vector3 orbitAxis = new Vector3(0, 1, 0.5f);
+    public Transform parent;
+    public Transform child;
     // Start is called before the first frame update
     void Start()
     {
-        
+        parent = transform.parent;
+        child = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        float speed = 36f;
+        float angle = speed * Time.deltaTime;
+        Debug.Log(angle);
+        child.RotateAround(parent.position, parent.forward, angle);
     }
 }

aa9fd3b993e65e6cdc101dae53754c646daef808
thedaruma <rgiorda1@gmail.com>
2023-09-04 16:34:19 -0400

    add ship acceleration

--- Assets/Scripts/Player/ShipMovement.cs
+++ Assets/Scripts/Player/ShipMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ShipMovement : MonoBehaviour
 {
     public float speed = 10;
+    public float acceleration = 100;
+
     private Vector2 velocity;
     // Start is called before the first frame update
     void Start()
@@ -15,12 +17,32 @@ public class ShipMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        // float h = Input.GetAxis("Horizontal");
+        // float v = Input.GetAxis("Vertical");
 
-        velocity = new Vector2(h, v).normalized * speed;
+        // velocity = new Vector2(h, v).normalized * speed;
 
-        transform.position += new Vector3(0, velocity.y * Time.deltaTime, 0);
-        transform.position += new Vector3(velocity.x * Time.deltaTime, 0, 0);
+        // transform.position += new Vector3(0, velocity.y * Time.deltaTime, 0);
+        // transform.position += new Vector3(velocity.x * Time.deltaTime, 0, 0);
+
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        velocity = Vector2.Lerp(
+          velocity,
+          velocity + (input * acceleration * Time.deltaTime),
+          acceleration * Time.deltaTime
+        );
+
+        transform.position += new Vector3(
+            velocity.x * Time.deltaTime,
+            velocity.y * Time.deltaTime,
+            0
+        );
+
+        velocity *= 0.99f;
+        if (velocity.magnitude > speed)
+        {
+            velocity = velocity.normalized * speed;
+        };
     }
 }
